2543b1908af329bc209cb19613663d822d5e2ef9
Divyag29-07 <113122824+Divyag29-07@users.noreply.github.com>
2023-01-27 15:41:53 +0530

    Add files via upload

---
+++ inheritance1/src/com/Divya/packages/Testclass.java
@@ -0,0 +1,20 @@
+package com.divya.packages;
+
+
+public class Testclass {
+	
+	
+	
+	
+	
+	
+
+        public static void main(String[] args) {
+            Hlo h = new Hlo();
+            h.design();
+        }
+
+
+
+
+}
